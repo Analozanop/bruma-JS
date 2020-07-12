fd9ebc9377fd1d850ea34e21284ef6d327ccecf4
Ramiro D'Accorso <ramiro.daccorso@gmail.com>
2020-07-12 11:26:12 -0300

    Fijate que te agregue un metodo para mostrar tus elementos con javascript y los borre del html, usa eso como base y mejoralo! :D

--- app.js
+++ app.js
@@ -12,6 +12,33 @@ $("#hide").click(function(){
     });
   });
 
+  const contenedor = document.querySelector('#contenedorDeProductos');
+
+  function cargarItems(){
+      fetch('./data.JSON')
+          .then(respuesta => respuesta.json()) 
+          .then(productos => {
+              productos.forEach(prod => {
+                  const item = document.createElement('div');
+                  item.classList.add('col,s12,m6,xl4');
+                  item.innerHTML += `
+                    <div class="card">
+                      <div class="card-image">
+                          <img src="${prod.imagen}" alt="${prod.descripcion}">
+                      </div>
+                      <div class="card-content">
+                        <span class="card-title">${prod.titulo}</span>
+                      </div>
+                      <button class="btn waves-effect" type="submit" id="${prod.id}" onclick="alert('hola')">COMPRAR</button>
+                    </div>
+                  `;
+                  contenedor.appendChild(item);                
+              });
+          }) 
+          .catch(error => console.log('Hubo un error : ' + error.message))
+  }
+  
   $(document).ready(function(){
+    cargarItems();
     $('.sidenav').sidenav();
   });
